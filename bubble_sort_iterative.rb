b1faca85ea5a6566ef7166b1d127c9eb6ce55829
Andrew Wortham <awortham@worthams.net>
2015-08-03 19:50:05 -0400

    bubble sort

---
+++ bubble_sort_iterative.rb
@@ -0,0 +1,31 @@
+def create_array
+  (1..1000).to_a.shuffle
+end
+
+def bubble_sort_iterative(array)
+  new_array = array
+  swaps = 0
+  first_number_in_pair = 0
+  (new_array.count - 1).times do
+    if new_array[first_number_in_pair] < new_array[first_number_in_pair + 1]
+      new_array[first_number_in_pair], new_array[first_number_in_pair + 1] =
+      new_array[first_number_in_pair + 1], new_array[first_number_in_pair]
+      swaps += 1
+    end
+    first_number_in_pair += 1
+  end
+  if swaps > 0
+    return true
+  end
+end
+
+shuffled_array = create_array
+$sorted = false
+x = Time.new
+until $sorted
+  $sorted = bubble_sort_iterative(shuffled_array)
+end
+y = Time.new
+
+
+puts y - x
---
+++ bubble_sort_recurssive.rb
@@ -0,0 +1,30 @@
+def create_array
+  (1..1000).to_a.shuffle
+end
+
+def bubble_sort_recurssive(array)
+  $x = Time.new
+  new_array = array
+  swaps = 0
+  first_number_in_pair = 0
+  (new_array.count - 1).times do
+    if new_array[first_number_in_pair] < new_array[first_number_in_pair + 1]
+      new_array[first_number_in_pair], new_array[first_number_in_pair + 1] =
+      new_array[first_number_in_pair + 1], new_array[first_number_in_pair]
+      swaps += 1
+    end
+    first_number_in_pair += 1
+  end
+  if swaps > 0
+    bubble_sort_recurssive(new_array)
+    return ""
+  else
+    puts new_array
+    $y = Time.new
+  end
+end
+
+
+bubble_sort_recurssive(create_array)
+
+puts $y - $x
